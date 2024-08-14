14e24a0cefd3b7fcbf7e501a236735e8c8d6c4d0
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-08-13 21:42:10 -0500

    Task-1008: Funcionalidad User Create

--- Controllers/Users/UsersController.cs
+++ Controllers/Users/UsersController.cs
@@ -28,5 +28,33 @@ namespace CelsiaAssetsment.Controllers
             return View(users);
         }
 
+        public IActionResult Create()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Auth");
+            }
+
+            return View();
+        }
+
+        public IActionResult CreateUser(User user)
+        {
+            try
+            {
+                if (!User.Identity.IsAuthenticated)
+                {
+                    return RedirectToAction("Index", "Auth");
+                }
+
+                _userRepository.CreateUser(user);
+
+                return RedirectToAction("Index", "Users");
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Error al crear el usuario: {e.Message}");
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
